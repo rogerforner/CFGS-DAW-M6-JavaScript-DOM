ca6069710e9ab8ea9165c5b05a7d712a0ddbc624
Roger Forner Fabre <hello@rogerforner.com>
2018-03-17 10:46:00 +0100

    Fins punt 4 OK

--- assets/js/roger.js
+++ assets/js/roger.js
@@ -2,18 +2,31 @@ $(function() {
   /*
   1. ús del document.getElementById()
   *****************************************************************************/
+  // Seleccionem l'element del nav amb ID "roger" i el subratllem aplicant un
+  // style="textDecoration:underline;"
+  document.getElementById("roger").style.textDecoration = "underline";
 
   /*
   2.ús del document.getElementsByTagName()
   *****************************************************************************/
+  // Seleccionem el primer element del contingut <p> i justifiquem el text.
+  // getElementsByTagName() ens retorna un array.
+  document.getElementsByTagName("p")[0].style.textAlign = "justify";
 
   /*
   3. saber accedir a atributs HTML del DOM.
   *****************************************************************************/
+  // Obtenir el primer atribut de la primera etiqueta <meta> del <head>, la qual
+  // es correspon a la codificació de caràcters del document UTF8.
+  // Desprès mostrem el resultat amb un Alert.
+  var attrMetaCharset = document.getElementsByTagName("meta")[0].attributes[0].value;
+  alert("La codificació de caràcters del document és "+attrMetaCharset);
 
   /*
   4. saber usar la propietat innerHTML.
   *****************************************************************************/
+  // Dintre de <p> hi ha un <pan id="eshared"> en el que hi afegim el text següent.
+  document.getElementById("eshared").innerHTML = " eShared és un entorn virtual on es recullen apunts dels diferents àmbits educatius per tal de facilitar l’estudi dels/les estudiants i proporcionar una guia, o millor dit, una eina, sempre actualitzada, per als/les professionals d’un sector educatiu que es renova de forma constant"
 
   /*
   5. saber crear nous elements al DOM (<p>, <img>, etc.)
